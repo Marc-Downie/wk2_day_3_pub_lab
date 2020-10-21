1c1c748a891eb61d40d3d022a2c537b28cecbbd3
Ian F <ian.fiddeman@gmail.com>
2020-10-21 12:20:01 +0100

    third commit

--- src/pub.py
+++ src/pub.py
@@ -2,8 +2,15 @@ class Pub:
     def __init__(self, name, till):
         self.name = name
         self.till = till
+        self.check = False
     
     def increase_till(self, drinks):
         self.till += drinks.price
     
+    def check_age(self, customer):
+        #check the customers age 
+        #check they are at least 18
+        #update self.check so it returns True
+        if customer.age >= 18:
+            self.check = True
     
--- tests/pub_test.py
+++ tests/pub_test.py
@@ -1,6 +1,7 @@
 import unittest
 from src.pub import Pub
 from src.drinks import Drinks
+from src.customer import Customer
 
 class TestPub(unittest.TestCase):
     
@@ -14,3 +15,13 @@ class TestPub(unittest.TestCase):
         drinks = Drinks("Wine", 3.00)
         self.pub.increase_till(drinks)
         self.assertEqual(103.00, self.pub.till)
+
+    def test_customer_age__return_False(self):
+        customer = Customer("Keith", 20.00, 17)
+        self.pub.check_age(customer)
+        self.assertEqual(False, self.pub.check)
+
+    def test_customer_age__return_True(self):
+        customer = Customer("Jane", 20.00, 19)
+        self.pub.check_age(customer)
+        self.assertEqual(True, self.pub.check)
